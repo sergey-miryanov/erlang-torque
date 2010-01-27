541c7840a1f465f7603c3e26d84b1e4bb7f7aa97
sergey-miryanov <sergey.miryanov@gmail.com>
2010-01-27 21:26:02 +0500

    Add 'job_stat.erl' example

---
+++ examples/job_stat.erl
@@ -0,0 +1,20 @@
+#!/usr/bin/env escript
+%%! -pa ../ebin -pa ebin -sname job_stat
+
+print_stat (Stat) ->
+  if 
+  (length (Stat) == 2) -> 
+    [Name | Value] = Stat, 
+    io:format ("name: ~p, value = ~p~n", [Name, Value]);
+  (length (Stat) == 3) ->
+    [Name | [Resource | Value]] = Stat,
+    io:format ("name: ~p, value = ~p, resource: ~p~n", [Name, Value, Resource])
+  end.
+
+main (_) ->
+
+  torque:start_link ("casper"),
+  {ok, List} = torque:job_stat ("66.casper"),
+  lists:foreach (fun (A) -> print_stat (A) end, List),
+
+  ok.
